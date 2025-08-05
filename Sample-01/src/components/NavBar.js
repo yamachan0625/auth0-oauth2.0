910b6e5c1e3d3db7a31a4222d6631901f0081339
yamachan0625 <55592297+yamachan0625@users.noreply.github.com>
2025-08-05 11:49:22 +0900

    1

--- Sample-01/src/components/NavBar.js
+++ Sample-01/src/components/NavBar.js
@@ -22,19 +22,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const {
-    user,
-    isAuthenticated,
-    loginWithRedirect,
-    logout,
-  } = useAuth0();
+  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
   const toggle = () => setIsOpen(!isOpen);
 
   const logoutWithRedirect = () =>
     logout({
-        logoutParams: {
-          returnTo: window.location.origin,
-        }
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
     });
 
   return (
